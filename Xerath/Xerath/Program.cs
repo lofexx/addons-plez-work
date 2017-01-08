b4832dd022fb8b4fc59ca3a41c5344f96b81557b
lofexx <lofbom12@gmail.com>
2017-01-09 00:55:20 +0100

    changed some spaghetti
    spaghetti fix

--- Xerath/Xerath/Program.cs
+++ Xerath/Xerath/Program.cs
@@ -3,7 +3,7 @@ using EloBuddy;
 using EloBuddy.SDK;
 using EloBuddy.SDK.Events;
 using EloBuddy.SDK.Menu.Values;
-    
+    //why are u checking out my spaghetti Zaloopa?
 
 namespace Xerath
 {
@@ -25,12 +25,16 @@ namespace Xerath
             //this prints my shit on the chat
             Game.OnTick += OnTick;
             //uses ontick
+
+            Spells.InitializeSpells();
+            Menus.CreateMenu();
+
+            //fuck my life
         }
 
         private static void OnTick(EventArgs args)
         {
-            Spells.InitializeSpells();
-            Menus.CreateMenu();
+           
 
             if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo)) ExecuteCombo();
         }
@@ -41,3 +45,7 @@ namespace Xerath
         }
     }
 }
+//paona is a pleb
+
+    //report melanie for doxing me
+    //Wanna swap name yorik100?
